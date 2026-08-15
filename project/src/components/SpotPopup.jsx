46b29ccb1add64bd9abd5517242b6d5d5f3239c3
Tomizawa-laptop <m.tomizawa821@gmail.com>
2026-08-15 21:57:31 +0900

    SpotPopupコンポーネントに画像表示とタグスタイルを追加

--- project/src/components/SpotPopup.jsx
+++ project/src/components/SpotPopup.jsx
@@ -6,11 +6,11 @@ function SpotPopup ({zawaSpot}) {
             <h3>{zawaSpot.prefecture}</h3>
             <h3>{zawaSpot.postalCode}</h3>
             <h3>{zawaSpot.description}</h3>
-            <h3>{zawaSpot.image}</h3>
+            <h3><img src={zawaSpot.image} alt={zawaSpot.name} className="w-full h-32 object-cover rounded" /></h3>
             <h3>
                 {/* zawaSpot（propsで受け取ったオブジェクト）の中のtag配列をmap(受け皿名)で処理 */}
                 {zawaSpot.tags.map((OriginalNameTag) => (
-                    <span key={OriginalNameTag}>{OriginalNameTag}</span>
+                    <span key={OriginalNameTag} className="mr-2 text-sm text-gray-500">#{OriginalNameTag}</span>
                 ))}
             </h3>
             <h3>{zawaSpot.createdAt}</h3>
