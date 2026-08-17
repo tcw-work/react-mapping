1b1819e0b9b7cd4e928dae6337d3bcd155bb728a
Tomizawa-laptop <m.tomizawa821@gmail.com>
2026-08-17 19:58:41 +0900

    usestate分割代入

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,13 +1,19 @@
 import MapView from './components/MapView.jsx'
 import SpotList from './components/SpotList.jsx'
 import spotData from './data/spots.json'
+// usestate
+import { useState } from 'react'
 
 function App() {
+  //現在の値,値を更新するための関数=初期値
+  // useState()は必ず「[現在の値, 更新関数]という2つだけの配列」を、この順番で返す
+  const[selectedSpotId,setSelectedId] = useState(null)
+
   return (
     // 返せる戻り値は一つだけなのでdivで囲む
     <div className="flex flex-col md:flex-row h-screen w-screen overflow-hidden">
-      <SpotList spot={spotData}/>
-      <MapView spot={spotData} />
+      <SpotList spot={spotData} selectedSpotId={selectedSpotId} onSelectSpot={setSelectedId}/>
+      <MapView spot={spotData} selectedSpotId={selectedSpotId} />
     </div>
   )
 }
--- project/src/components/SpotList.jsx
+++ project/src/components/SpotList.jsx
@@ -1,4 +1,5 @@
-function SpotList ({spot}) {
+// propsを分割代入
+function SpotList ({spot, selectedSpotId, onSelectSpot}) {
     return(
         <aside className="w-full md:w-72 h-48 md:h-full overflow-y-auto border-b md:border-b-0 md:border-r border-gray-200 bg-white shrink-0">
             <h2 className="sticky top-0 bg-white px-4 py-3 text-sm font-semibold text-gray-500 border-b border-gray-200">
@@ -6,7 +7,12 @@ function SpotList ({spot}) {
             </h2>
             <ul className="divide-y divide-gray-100">
                 {spot.map((OriginalName) => (
-                    <li key={OriginalName.id} className="px-4 py-3 hover:bg-gray-50 cursor-pointer transition-colors">
+                    // クリックしたらApp.jsxから渡された**setSelectedId（useStateの更新関数）を実行
+                    <li key={OriginalName.id} onClick={() => onSelectSpot(OriginalName.id)}
+                    className={`px-4 py-3 hover:bg-gray-50 cursor-pointer transition-colors ${
+                        OriginalName.id === selectedSpotId ? 'bg-blue-100' : 'hover:bg-gray-50'
+                        }`}
+                    >
                         <p className="font-medium text-gray-900">{OriginalName.name}</p>
                         <p className="text-xs text-gray-500">{OriginalName.category}・{OriginalName.prefecture}</p>
                     </li>
