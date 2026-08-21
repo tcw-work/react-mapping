f6eada3d390d0c588902dc66930b356bfe4913be
Tomizawa-laptop <m.tomizawa821@gmail.com>
2026-08-21 14:08:34 +0900

    カテゴリーをfilterで操作

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -8,12 +8,20 @@ function App() {
   //現在の値,値を更新するための関数=初期値
   // useState()は必ず「[現在の値, 更新関数]という2つだけの配列」を、この順番で返す
   const[selectedSpotId,setSelectedId] = useState(null)
+  const[categoryFilter, setCategoryFilter] =useState('all')
 
+  // これだと初期値の「all」という文字列と一致した場合のみ表示される
+  // const filteredSpots = spotData.filter((s) => s.category === categoryFilter);
+
+  // categoryFilter（現在選ばれているカテゴリ）が "all" と一致するか、が条件
+  // 一致する（true）→ 絞り込みなしで全件（spotData）
+  // 一致しない（false）→ カテゴリで絞り込む（.filter()）
+  const filteredSpots = categoryFilter === 'all' ? spotData : spotData.filter((s) => s.category === categoryFilter)
   return (
     // 返せる戻り値は一つだけなのでdivで囲む
     <div className="flex flex-col md:flex-row h-screen w-screen overflow-hidden">
-      <SpotList spot={spotData} selectedSpotId={selectedSpotId} onSelectSpot={setSelectedId}/>
-      <MapView spot={spotData} selectedSpotId={selectedSpotId} />
+      <SpotList spot={filteredSpots} categoryFilter={categoryFilter} setCategoryFilter={setCategoryFilter} selectedSpotId={selectedSpotId} onSelectSpot={setSelectedId}/>
+      <MapView spot={filteredSpots} selectedSpotId={selectedSpotId} />
     </div>
   )
 }
--- project/src/components/SpotList.jsx
+++ project/src/components/SpotList.jsx
@@ -1,10 +1,18 @@
 // propsを分割代入
-function SpotList ({spot, selectedSpotId, onSelectSpot}) {
+function SpotList ({spot, selectedSpotId, onSelectSpot, categoryFilter, setCategoryFilter}) {
     return(
         <aside className="w-full md:w-72 h-48 md:h-full overflow-y-auto border-b md:border-b-0 md:border-r border-gray-200 bg-white shrink-0">
             <h2 className="sticky top-0 bg-white px-4 py-3 text-sm font-semibold text-gray-500 border-b border-gray-200">
                 スポット一覧（{spot.length}件）
             </h2>
+
+            <select value={categoryFilter} onChange={(e) => setCategoryFilter(e.target.value)} name="" id="">
+                <option value="all">すべて</option>
+                <option value="洞窟">洞窟</option>
+                <option value="遺跡">遺跡</option>
+                <option value="falseフラグ">falseフラグ</option>{/* わざとfalse */}
+            </select>
+
             <ul className="divide-y divide-gray-100">
                 {spot.map((OriginalName) => (
                     // クリックしたらApp.jsxから渡された**setSelectedId（useStateの更新関数）を実行
