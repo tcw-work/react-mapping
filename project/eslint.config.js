7f5146b8849179205dd948cb22800826f3aafad1
Tomizawa-laptop <m.tomizawa821@gmail.com>
2026-08-21 17:02:00 +0900

    TSセットアップ

--- project/eslint.config.js
+++ project/eslint.config.js
@@ -3,15 +3,17 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import tseslint from 'typescript-eslint'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      tseslint.configs.recommended,
     ],
     languageOptions: {
       globals: globals.browser,
